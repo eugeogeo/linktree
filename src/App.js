ad41097731ab4029b11d2aa58632c7b07e08594b
Geovana <geovanaf1912@gmail.com>
2022-05-01 23:51:00 -0300

    inicio ok, verificar problemas da MUI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="geral">
+
+      <div className="head">
+
+        <div className="picture">
+          {/*arruma o MUI */}
+
+                  
+          {/* <Avatar 
+            alt="fotinha" 
+            sx={{ width: 100, height: 100 }}
+            src="https://yt3.ggpht.com/ytc/AKedOLRJFxojUGx7u06lBjilcCYrHQyLt9k678A5Uef2=s900-c-k-c0x00ffffff-no-rj" 
+          />   */}
+            
+          <img className="icon" src = "https://yt3.ggpht.com/ytc/AKedOLRJFxojUGx7u06lBjilcCYrHQyLt9k678A5Uef2=s900-c-k-c0x00ffffff-no-rj"></img>
+          
+        </div>
+
+        <div className="infos">
+          <div className="username">
+            @marvel
+          </div>
+          <div className="slogan">
+            escrever um slogan aqui 
+          </div>
+        </div>
+
+      </div>
+
+      {/* <div className="links">
+        <Button variant="contained">Contained</Button>
+      </div> */}
     </div>
   );
 }
